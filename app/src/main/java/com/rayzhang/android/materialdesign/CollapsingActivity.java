24e452db0648bf55c0da30f44a58da5fcde97eea
ray00178 <ray00178@gmail.com>
2017-03-12 14:28:52 +0800

    add AppBarLayout & CollaspingToolBarLayout

---
+++ app/src/main/java/com/rayzhang/android/materialdesign/CollapsingActivity.java
@@ -0,0 +1,76 @@
+package com.rayzhang.android.materialdesign;
+
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+
+import com.rayzhang.android.materialdesign.adapter.DemoAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CollapsingActivity extends AppCompatActivity {
+    /**
+     * CollapsingToolbarLayout use
+     */
+    private AppBarLayout mAppBarLayout;
+    private CollapsingToolbarLayout mCollapsLayout;
+    private Toolbar mToolBar;
+    private RecyclerView mRecyView;
+
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_collapsing);
+
+        mAppBarLayout = (AppBarLayout) findViewById(R.id.mAppBarLayout);
+        mCollapsLayout = (CollapsingToolbarLayout) findViewById(R.id.mCollaspLayout);
+        mToolBar = (Toolbar) findViewById(R.id.mToolBar);
+        setSupportActionBar(mToolBar);
+
+        /*mAppBarLayout.addOnOffsetChangedListener(new AppBarLayout.OnOffsetChangedListener() {
+            // https://codedump.io/share/x8JwlFS66glv/1/show-collapsingtoolbarlayout-title-only-when-collapsed
+            boolean isShow = false;
+            int scrollRange = -1;
+
+            @Override
+            public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
+                if (scrollRange == -1) scrollRange = appBarLayout.getTotalScrollRange();
+                if (scrollRange + verticalOffset == 0) {
+                    mToolBar.setTitle("CollapsingToolbarLayout");
+                    isShow = true;
+                } else if (isShow) {
+                    mToolBar.setTitle("");
+                    isShow = false;
+                }
+            }
+        });*/
+        mCollapsLayout.setTitle("Collapsing");
+        mCollapsLayout.setCollapsedTitleTypeface(Typeface.SERIF);
+        mCollapsLayout.setCollapsedTitleTextColor(Color.WHITE);
+        mCollapsLayout.setExpandedTitleTypeface(Typeface.SERIF);
+        mCollapsLayout.setExpandedTitleColor(Color.TRANSPARENT);
+
+        mRecyView = (RecyclerView) findViewById(R.id.mRecyView);
+        mRecyView.setItemAnimator(new DefaultItemAnimator());
+        mRecyView.setHasFixedSize(true);
+        RecyclerView.LayoutManager manager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+        mRecyView.setLayoutManager(manager);
+        /**
+         * 設置RecyclerView的Adapter
+         */
+        List<String> list = new ArrayList<>();
+        for (int i = 1, j = 100; i <= j; i++) {
+            list.add("item:" + i);
+        }
+        DemoAdapter adapter = new DemoAdapter(list);
+        mRecyView.setAdapter(adapter);
+    }
+}
---
+++ app/src/main/java/com/rayzhang/android/materialdesign/adapter/DemoAdapter.java
@@ -0,0 +1,59 @@
+package com.rayzhang.android.materialdesign.adapter;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.rayzhang.android.materialdesign.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Ray on 2017/3/12.
+ */
+
+public class DemoAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+    private static final int NORMAL_ITEM = 9999;
+    private ArrayList<String> list;
+
+    public DemoAdapter(List<String> list) {
+        this.list = new ArrayList<>();
+        this.list.addAll(list);
+    }
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.adapter_item, parent, false);
+        return new DemoNView(view);
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        int viewType = getItemViewType(position);
+        if (viewType == NORMAL_ITEM) {
+            ((DemoNView) holder).mTextView.setText(list.get(position));
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return NORMAL_ITEM;
+    }
+
+    public class DemoNView extends RecyclerView.ViewHolder {
+        private TextView mTextView;
+
+        public DemoNView(View itemView) {
+            super(itemView);
+            mTextView = (TextView) itemView.findViewById(R.id.mTextView);
+        }
+    }
+}
